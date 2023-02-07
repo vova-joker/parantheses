eeaa289630addb59591130c4bc6d5c4e9c9a0187
Vova <vova.joker.1978@mail.ru>
2023-02-07 19:51:45 +0300

    )

--- src/test/java/org/example/CheckFullParenthesesTest.java
+++ src/test/java/org/example/CheckFullParenthesesTest.java
@@ -13,17 +13,20 @@ public class CheckFullParenthesesTest extends TestCase {
         String expected2 = "6 - (()())";
         String expected3 = "0";
         String expected4 = "10 - ()(()())()";
+        String expected5 = "0";
         CheckFullParentheses checkFullParentheses = new CheckFullParentheses();
         String actual0 = checkFullParentheses.checkFullParentheses("(()");
         String actual1 = checkFullParentheses.checkFullParentheses(")()())");
         String actual2 = checkFullParentheses.checkFullParentheses(")(()())");
         String actual3 = checkFullParentheses.checkFullParentheses(")(");
         String actual4 = checkFullParentheses.checkFullParentheses("())(()())(()");
+        String actual5 = checkFullParentheses.checkFullParentheses(")");
         Assert.assertEquals(expected0, actual0);
         Assert.assertEquals(expected1, actual1);
         Assert.assertEquals(expected2, actual2);
         Assert.assertEquals(expected3, actual3);
         Assert.assertEquals(expected4, actual4);
+        Assert.assertEquals(expected5, actual5);
     }
 
 }
